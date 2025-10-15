221fed3ad4ad24063b0080a7febcd153fcf61c8e
Marchenko Dmitriy <dimamarch2014@gmail.com>
2025-10-15 20:00:51 +0500

    Сделал 3 практическую работу

---
+++ src/main/java/ru/marchenko/NauJava/config/BeanConfig.java
@@ -0,0 +1,48 @@
+package ru.marchenko.NauJava.config;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import ru.marchenko.NauJava.entity.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+    * Конфигурационный класс для создания и настройки бина taskContainer.
+ */
+@Configuration
+public class BeanConfig {
+
+    /**
+     * Название приложения, загружаемое из файла настроек (application.properties).
+     */
+    @Value("${app.name}")
+    private String appName;
+
+    /**
+     * Версия приложения, загружаемая из файла настроек (application.properties).
+     */
+    @Value("${app.version}")
+    private String appVersion;
+
+    /**
+     * Создает и возвращает бин taskContainer, представляющий собой список задач.
+     * Этот бин имеет синглтон область видимости, что означает, что будет создан
+     * только один экземпляр этого списка на все приложение.
+     * @return Список задач (List<Task>).
+     */
+    @Bean
+    @Scope(value = BeanDefinition.SCOPE_SINGLETON)
+    public List<Task> taskContainer() {
+        return new ArrayList<>();
+    }
+    @PostConstruct
+    public void init() {
+        System.out.println("Application Name: " + appName);
+        System.out.println("Application Version: " + appVersion);
+    }
+}
---
+++ src/main/java/ru/marchenko/NauJava/entity/TaskStatus.java
@@ -0,0 +1,10 @@
+package ru.marchenko.NauJava.entity;
+
+/**
+ * Перечисление статусов задачи.
+ */
+public enum TaskStatus {
+    BACKLOG,
+    IN_PROGRESS,
+    DONE
+}
